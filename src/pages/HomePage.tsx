c97ee7905b76140b87a73414e1c09dee3f02fd64
EvangeRodriguez <eva.irodriguez93@gmail.com>
2024-03-20 12:01:42 +0100

    react organization

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+      <div className='inicio'>
+        <div className= "inicio">  
+         Fiesta 15 Martina 
+        </div>      </div>
+    )
+  }
+  

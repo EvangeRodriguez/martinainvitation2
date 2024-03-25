1006484dc3668dbe6c6ef0942980f55d7d37490f
EvangeRodriguez <eva.irodriguez93@gmail.com>
2024-03-25 17:05:37 +0100

    modificación index

--- src/components/Formulario/Forms.tsx
+++ src/components/Formulario/Forms.tsx
@@ -1,11 +1,19 @@
+import './Forms.css';
 
 
 export default function Forms() {
+  const redirectToGoogleForms = () => {
+    window.location.href = 'https://docs.google.com/forms/u/0/';
+  };
+
   return (
     <div>
-      <p >
-      BOTONNN
-         </p>
+      <img
+        src="buton.png"
+        alt="Confirmar"
+        onClick={redirectToGoogleForms}
+        className="boton"
+      />
     </div>
-  )
+  );
 }

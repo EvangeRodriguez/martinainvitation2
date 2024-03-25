eab37cdfaae39393999f3f5375294bb7896a519c
EvangeRodriguez <eva.irodriguez93@gmail.com>
2024-03-25 17:15:58 +0100

    link google maps

--- src/components/Direccion/Direccion.tsx
+++ src/components/Direccion/Direccion.tsx
@@ -1,16 +1,22 @@
 import './Direccion.css'
 
 export default function Direccion() {
+  const redirectToMap = () => {
+    window.open('https://maps.app.goo.gl/xLGS8Zu4yPnsfZY49', '_blank');
+  }; 
+
     return (
       <div>
-        <div className="direccion" style={{ display: "flex", alignItems: "center" }}>
-          <img src="maps.png" alt="mapa" style={{ marginRight: "10px" }} />
-          <p className='salon'>
-            Casona La Coqueta
-            RN19 316, Monte Cristo
-          </p>
-        </div>
+         <div className="direccion" style={{ display: "flex", alignItems: "center" }}>
+        <a href="https://maps.app.goo.gl/xLGS8Zu4yPnsfZY49" target="_blank" rel="noopener noreferrer" onClick={(e) => e.preventDefault()}>
+          <img src="maps.png" alt="mapa" style={{ marginRight: "10px", cursor: "pointer" }} onClick={redirectToMap} />
+        </a>
+        <p className='salon'>
+          Casona La Coqueta
+          RN19 316, Monte Cristo
+        </p>
       </div>
-    );
+    </div>
+  );
   }
-  
+ 

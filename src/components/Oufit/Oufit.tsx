a3a8607a1f72d1fb2a271497394239d0e97008fa
EvangeRodriguez <eva.irodriguez93@gmail.com>
2024-04-30 11:18:37 +0200

    modificaciones logos

--- src/components/Oufit/Oufit.tsx
+++ src/components/Oufit/Oufit.tsx
@@ -4,7 +4,7 @@ export default function Oufit() {
   return (
     <div>
       <div className="outfit" style={{ display: "flex", alignItems: "center" }}>
-        <img src="oufit.png" alt="outfit" className="map-image"/>
+        <img src="oufit1.png" alt="outfit" className="map-image"/>
         <p className='ropa'>
           Outfit: Formal
         </p>
